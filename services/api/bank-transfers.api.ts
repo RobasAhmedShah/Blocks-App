b3ed8bf709ddc089774a62761616356071372936
itxsamad1 <itxsamad@icloud.com>
2025-12-22 10:58:57 +0500

    Remove unnecessary blank lines in KycApprovedScreen, bankTransfersAPI, and bankWithdrawalsAPI files

--- services/api/bank-transfers.api.ts
+++ services/api/bank-transfers.api.ts
@@ -117,3 +117,5 @@ export const bankTransfersAPI = {
     return response.data.signedUrl;
   },
 };
+
+
--- services/api/bank-withdrawals.api.ts
+++ services/api/bank-withdrawals.api.ts
@@ -66,3 +66,5 @@ export const bankWithdrawalsAPI = {
     return response.data;
   },
 };
+
+
